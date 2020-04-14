6c2302cfbeca180fe5d96818b9165e26b1c3c19b
Alejandro Redondo García <aredondosy@alumnos.unex.es>
2020-04-14 16:42:03 +0200

    Version de VREP en M.E.

---
+++ components/vrepControlME/src/genericworker.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, Ice, os
+from PySide2 import QtWidgets, QtCore
+
+ROBOCOMP = ''
+try:
+	ROBOCOMP = os.environ['ROBOCOMP']
+except KeyError:
+	print('$ROBOCOMP environment variable not set, using the default value /opt/robocomp')
+	ROBOCOMP = '/opt/robocomp'
+
+preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ --all /opt/robocomp/interfaces/"
+Ice.loadSlice(preStr+"CommonBehavior.ice")
+import RoboCompCommonBehavior
+
+additionalPathStr = ''
+icePaths = [ '/opt/robocomp/interfaces' ]
+try:
+	SLICE_PATH = os.environ['SLICE_PATH'].split(':')
+	for p in SLICE_PATH:
+		icePaths.append(p)
+		additionalPathStr += ' -I' + p + ' '
+	icePaths.append('/opt/robocomp/interfaces')
+except:
+	print('SLICE_PATH environment variable was not exported. Using only the default paths')
+	pass
+
+ice_CameraRGBDSimpleYoloPub = False
+for p in icePaths:
+	if os.path.isfile(p+'/CameraRGBDSimpleYoloPub.ice'):
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"CameraRGBDSimpleYoloPub.ice"
+		Ice.loadSlice(wholeStr)
+		ice_CameraRGBDSimpleYoloPub = True
+		break
+if not ice_CameraRGBDSimpleYoloPub:
+	print('Couln\'t load CameraRGBDSimpleYoloPub')
+	sys.exit(-1)
+from RoboCompCameraRGBDSimpleYoloPub import *
+ice_JoystickAdapter = False
+for p in icePaths:
+	if os.path.isfile(p+'/JoystickAdapter.ice'):
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"JoystickAdapter.ice"
+		Ice.loadSlice(wholeStr)
+		ice_JoystickAdapter = True
+		break
+if not ice_JoystickAdapter:
+	print('Couln\'t load JoystickAdapter')
+	sys.exit(-1)
+from RoboCompJoystickAdapter import *
+ice_YoloServer = False
+for p in icePaths:
+	if os.path.isfile(p+'/YoloServer.ice'):
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"YoloServer.ice"
+		Ice.loadSlice(wholeStr)
+		ice_YoloServer = True
+		break
+if not ice_YoloServer:
+	print('Couln\'t load YoloServer')
+	sys.exit(-1)
+from RoboCompYoloServer import *
+ice_CameraRGBDSimple = False
+for p in icePaths:
+	if os.path.isfile(p+'/CameraRGBDSimple.ice'):
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"CameraRGBDSimple.ice"
+		Ice.loadSlice(wholeStr)
+		ice_CameraRGBDSimple = True
+		break
+if not ice_CameraRGBDSimple:
+	print('Couln\'t load CameraRGBDSimple')
+	sys.exit(-1)
+from RoboCompCameraRGBDSimple import *
+ice_AprilTagsServer = False
+for p in icePaths:
+	if os.path.isfile(p+'/AprilTagsServer.ice'):
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"AprilTagsServer.ice"
+		Ice.loadSlice(wholeStr)
+		ice_AprilTagsServer = True
+		break
+if not ice_AprilTagsServer:
+	print('Couln\'t load AprilTagsServer')
+	sys.exit(-1)
+from RoboCompAprilTagsServer import *
+
+
+from camerargbdsimpleyolopubI import *
+from joystickadapterI import *
+
+
+class GenericWorker(QtCore.QObject):
+
+	kill = QtCore.Signal()
+#Signals for State Machine
+	t_inicializar_to_detectarObjetos = QtCore.Signal()
+	t_detectarObjetos_to_detectarObjetos = QtCore.Signal()
+	t_detectarObjetos_to_moverBrazo = QtCore.Signal()
+	t_detectarObjetos_to_cogerObjeto = QtCore.Signal()
+	t_moverBrazo_to_detectarObjetos = QtCore.Signal()
+	t_moverBrazo_to_moverBrazo = QtCore.Signal()
+	t_moverBrazo_to_cogerObjeto = QtCore.Signal()
+	t_moverBrazo_to_dejarObjeto = QtCore.Signal()
+	t_cogerObjeto_to_moverBrazo = QtCore.Signal()
+	t_cogerObjeto_to_dejarObjeto = QtCore.Signal()
+	t_dejarObjeto_to_moverBrazo = QtCore.Signal()
+	t_dejarObjeto_to_finalizar = QtCore.Signal()
+
+#-------------------------
+
+	def __init__(self, mprx):
+		super(GenericWorker, self).__init__()
+
+
+		self.apriltagsserver_proxy = mprx["AprilTagsServerProxy"]
+
+		
+		self.mutex = QtCore.QMutex(QtCore.QMutex.Recursive)
+		self.Period = 	1000
+		self.timer = QtCore.QTimer(self)
+
+#State Machine
+		self.Maq1FirtsMovement= QtCore.QStateMachine()
+		self.detectarObjetos_state = QtCore.QState(self.Maq1FirtsMovement)
+		self.moverBrazo_state = QtCore.QState(self.Maq1FirtsMovement)
+		self.cogerObjeto_state = QtCore.QState(self.Maq1FirtsMovement)
+		self.dejarObjeto_state = QtCore.QState(self.Maq1FirtsMovement)
+		self.inicializar_state = QtCore.QState(self.Maq1FirtsMovement)
+
+		self.finalizar_state = QtCore.QFinalState(self.Maq1FirtsMovement)
+
+
+#------------------
+#Initialization State machine
+		self.inicializar_state.addTransition(self.t_inicializar_to_detectarObjetos, self.detectarObjetos_state)
+		self.detectarObjetos_state.addTransition(self.t_detectarObjetos_to_detectarObjetos, self.detectarObjetos_state)
+		self.detectarObjetos_state.addTransition(self.t_detectarObjetos_to_moverBrazo, self.moverBrazo_state)
+		self.detectarObjetos_state.addTransition(self.t_detectarObjetos_to_cogerObjeto, self.cogerObjeto_state)
+		self.moverBrazo_state.addTransition(self.t_moverBrazo_to_detectarObjetos, self.detectarObjetos_state)
+		self.moverBrazo_state.addTransition(self.t_moverBrazo_to_moverBrazo, self.moverBrazo_state)
+		self.moverBrazo_state.addTransition(self.t_moverBrazo_to_cogerObjeto, self.cogerObjeto_state)
+		self.moverBrazo_state.addTransition(self.t_moverBrazo_to_dejarObjeto, self.dejarObjeto_state)
+		self.cogerObjeto_state.addTransition(self.t_cogerObjeto_to_moverBrazo, self.moverBrazo_state)
+		self.cogerObjeto_state.addTransition(self.t_cogerObjeto_to_dejarObjeto, self.dejarObjeto_state)
+		self.dejarObjeto_state.addTransition(self.t_dejarObjeto_to_moverBrazo, self.moverBrazo_state)
+		self.dejarObjeto_state.addTransition(self.t_dejarObjeto_to_finalizar, self.finalizar_state)
+
+
+		self.detectarObjetos_state.entered.connect(self.sm_detectarObjetos)
+		self.moverBrazo_state.entered.connect(self.sm_moverBrazo)
+		self.cogerObjeto_state.entered.connect(self.sm_cogerObjeto)
+		self.dejarObjeto_state.entered.connect(self.sm_dejarObjeto)
+		self.inicializar_state.entered.connect(self.sm_inicializar)
+		self.finalizar_state.entered.connect(self.sm_finalizar)
+
+		self.Maq1FirtsMovement.setInitialState(self.inicializar_state)
+
+#------------------
+
+#Slots funtion State Machine
+	@QtCore.Slot()
+	def sm_detectarObjetos(self):
+		print("Error: lack sm_detectarObjetos in Specificworker")
+		sys.exit(-1)
+
+	@QtCore.Slot()
+	def sm_moverBrazo(self):
+		print("Error: lack sm_moverBrazo in Specificworker")
+		sys.exit(-1)
+
+	@QtCore.Slot()
+	def sm_cogerObjeto(self):
+		print("Error: lack sm_cogerObjeto in Specificworker")
+		sys.exit(-1)
+
+	@QtCore.Slot()
+	def sm_dejarObjeto(self):
+		print("Error: lack sm_dejarObjeto in Specificworker")
+		sys.exit(-1)
+
+	@QtCore.Slot()
+	def sm_inicializar(self):
+		print("Error: lack sm_inicializar in Specificworker")
+		sys.exit(-1)
+
+	@QtCore.Slot()
+	def sm_finalizar(self):
+		print("Error: lack sm_finalizar in Specificworker")
+		sys.exit(-1)
+
+
+#-------------------------
+	@QtCore.Slot()
+	def killYourSelf(self):
+		rDebug("Killing myself")
+		self.kill.emit()
+
+	# \brief Change compute period
+	# @param per Period in ms
+	@QtCore.Slot(int)
+	def setPeriod(self, p):
+		print("Period changed", p)
+		self.Period = p
+		self.timer.start(self.Period)
---
+++ components/vrepControlME/src/specificworker.py
@@ -0,0 +1,246 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from genericworker import *
+import cv2
+import math
+import time
+import numpy as np
+import vrep
+import b0RemoteApi
+import RoboCompCameraRGBDSimple as RoboCompCameraRGBDSimple
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.Period = 50
+		#self.timer.timeout.connect(self.detectarObjetos)
+		self.timer.start(self.Period)
+
+		# Connect to VREP IK
+		self.client = b0RemoteApi.RemoteApiClient('b0RemoteApi_pythonClient','b0RemoteApiAddOn')
+		self.target = self.client.simxGetObjectHandle('target', self.client.simxServiceCall())[1]
+		self.base = self.client.simxGetObjectHandle('gen3', self.client.simxServiceCall())[1]
+		self.grippers = self.client.simxGetObjectHandle('ROBOTIQ_85', self.client.simxServiceCall())[1]
+		#self.camera_arm = self.client.simxGetObjectHandle('Camera_Arm', self.client.simxServiceCall())[1]
+		self.camera_arm = self.client.simxGetObjectHandle('camera_in_hand', self.client.simxServiceCall())[1]
+		
+		#self.imageVREP = TImage()
+
+		self.Maq1FirtsMovement.start()
+		self.destroyed.connect(self.t_dejarObjeto_to_finalizar)
+
+		self.JOYSTICK_EVENT = False
+		print("Leaving Init")
+
+	def __del__(self):
+		print('SpecificWorker destructor')
+
+	def setParams(self, params):
+		return True
+
+	def detectCircles(self, orig):
+		gray = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+		# detect circles in the image
+
+		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.8, 100)
+		# ensure at least some circles were found
+		if circles is not None:
+			print("Detected circle")
+			# convert the (x, y) coordinates and radius of the circles to integers
+			circles = np.round(circles[0, :]).astype("int")
+			# loop over the (x, y) coordinates and radius of the circles
+			for (x, y, r) in circles:
+			# draw the circle in the output image, then draw a rectangle
+			# corresponding to the center of the circle
+				cv2.circle(orig, (x, y), r, (0, 255, 0), 4)
+				cv2.rectangle(orig, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+			# show the output image
+		return orig, circles
+			
+	########################################################################
+
+	@QtCore.Slot()	
+	def detectarObjetos(self):	
+		#posicion de reconocimiento
+		#self.client.simxSetObjectPose(self.target, self.base, [0.01, -0.35, 0.53, 0.0, 0.0, 0.0, 0.0], self.client.simxServiceCall())		
+		
+		# if self.JOYSTICK_EVENT:
+		# 	#self.client.simxSetObjectPosition('target', self.client.simxServiceCall())[1]
+		# 	self.JOYSTICK_EVENT = False
+
+		# capture image
+		res, resolution, imageVREP = self.client.simxGetVisionSensorImage(self.camera_arm, False, self.client.simxServiceCall())
+		img = np.fromstring(imageVREP, np.uint8).reshape( resolution[1],resolution[0], 3)
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+		img = cv2.flip(img, 0)
+		img, self.circles = self.detectCircles(img)
+		cv2.imshow("Gen3", img)
+		cv2.waitKey(1)
+
+		#change state?
+		if self.circles is not None:
+			return True
+		else:
+			return False
+			
+	def moverBrazo(self):
+		cx, cy, cr = self.circles[0]
+		target = self.client.simxGetObjectPose(self.target, self.base, self.client.simxServiceCall())[1]
+		objActual = np.array([cx, cy])
+		diffPos = np.array([320, 240.]) - objActual
+		# 	height = l.tz/1000.
+		print(diffPos)
+		try:
+			target[0] = target[0] + ( diffPos[0] * 0.0005 )
+			target[1] = target[1] - ( diffPos[1] * 0.0005 )
+			target[2] = target[2] - ( target[2] * 0.005 )
+			self.client.simxSetObjectPose(self.target, self.base, target, self.client.simxServiceCall())
+		except:
+			print("Something went wrong")
+
+		#change state ?
+		if target[2] < 0.1: #esta en posicion de cogerse
+			return True
+		else:
+			return False
+
+
+	def cogerObjeto(self):
+		#cerramos las pinzas
+		pass
+
+	def dejarObjeto(self):
+		#movemos el brazo a esa posicion objetivo
+		self.client.simxSetObjectPose(self.target, self.base, self.listCoordenadasObjeto, self.client.simxServiceCall())
+		#abrimos pinzas
+
+	def getAprilTags(self, image, resolution):
+		try:
+			frame = Image()
+			frame.data = image.flatten()
+			#frame.data = image.data
+			frame.frmt = Format(Mode.RGB888Packet, resolution[0], resolution[1], 3)
+			frame.timeStamp = time.time()
+			tags_list = self.apriltagsserver_proxy.getAprilTags(frame=frame, tagsize=80, mfx=554, mfy=554)
+			return tags_list
+	
+		except Ice.Exception as ex:
+			print(ex)
+
+# =============== Slots methods for State Machine ===================
+# ===================================================================
+	#
+	# sm_inicializar
+	#
+	@QtCore.Slot()
+	def sm_inicializar(self):
+		print("Entered state inicializar")
+		#self.client.simxSetObjectPose(self.target, self.base, [0.01, -0.35, 0.53, 0.0, 0.0, 180.0, 0.0], self.client.simxServiceCall())		
+		self.t_inicializar_to_detectarObjetos.emit()
+		pass
+
+	#
+	# sm_cogerObjeto
+	#
+	@QtCore.Slot()
+	def sm_cogerObjeto(self):
+		print("Entered state cogerObjeto")
+		self.cogerObjeto()
+		pass
+
+	#
+	# sm_dejarObjeto
+	#
+	@QtCore.Slot()
+	def sm_dejarObjeto(self):
+		print("Entered state dejarObjeto")
+		self.dejarObjeto()
+		pass
+
+	#
+	# sm_detectarObjetos
+	#
+	@QtCore.Slot()
+	def sm_detectarObjetos(self):
+		print("Entered state detectarObjetos")
+		if not self.detectarObjetos():
+			self.t_detectarObjetos_to_detectarObjetos.emit()
+		else:
+			self.t_detectarObjetos_to_moverBrazo.emit()
+		
+	# sm_moverBrazo
+	#
+	@QtCore.Slot()
+	def sm_moverBrazo(self):
+		print("Entered state moverBrazo")
+		if not self.moverBrazo():
+			self.t_moverBrazo_to_detectarObjetos.emit()
+		else:
+			self.t_moverBrazo_to_cogerObjeto.emit()
+
+	#
+	# sm_finalizar
+	#
+	@QtCore.Slot()
+	def sm_finalizar(self):
+		print("Entered state finalizar")
+		pass
+
+
+# =================================================================
+# =================================================================
+	#
+	# SUBSCRIPTION to pushRGBD method from CameraRGBDSimplePub interface
+	#
+	def CameraRGBDSimpleYoloPub_pushRGBDYolo(self, im, dep, objs):
+		#print ("New image ", len(im.image), im.cameraID)	
+		self.imageVREP = im
+
+	#
+	# SUBSCRIPTION to sendData method from JoystickAdapter interface
+	#
+	def JoystickAdapter_sendData(self, data):
+		#print("Data", data)
+		if hasattr(data,"buttons") and data.buttons[0].clicked == True:
+				self.joystickVector['x'] = 0
+				self.joystickVector['y'] = 0
+				self.joystickVector['z'] = 0
+				bM.cartesian_Position_movement(self.base, self.base_cyclic, 0)
+		else: 
+		
+			if abs([axis.value for axis in data.axes if axis.name == "x"][0]) < 0.2:
+				self.joystickVector['x'] = 0
+			else:
+				self.joystickVector['x'] = [axis.value for axis in data.axes if axis.name == "x"][0] / 10.0
+			
+			if abs([axis.value for axis in data.axes if axis.name == "y"][0]) < 0.2:
+				self.joystickVector['y'] = 0
+			else:
+				self.joystickVector['y'] = [axis.value for axis in data.axes if axis.name == "y"][0] / 10.0 
+			
+			if abs([axis.value for axis in data.axes if axis.name == "z"][0]) < 0.2:
+				self.joystickVector['z'] = 0
+			else:
+				self.joystickVector['z'] = [axis.value for axis in data.axes if axis.name == "z"][0] / 10.0
+			
+			self.JOYSTICK_EVENT = True
+
